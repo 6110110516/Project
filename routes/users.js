ebca65f132511eae6c8febaa7ed4c039f637a24c
อภิวุฒิ ดำแก้ว <6110110516@psu.ac.th>
2022-01-14 00:18:41 +0700

    4/2 commit 7.12

--- routes/users.js
+++ routes/users.js
@@ -7,25 +7,55 @@ var moment = require('moment');
 var ordersearch;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-
+let pack_id = req.query.pack_id
   try {
-      res.render('users/menu',{
-        order_id: req.query.order_id,
-      })
+      if(pack_id == null)
+        res.render('users/menu',{
+          order_id: req.query.pack_id,
+        })
+      else
+        res.redirect('/users/check?pack_id='+pack_id)  
+
   } catch(e){
       next(e);
   }
-});
+}); 
+
+router.get('/check',(req, res, next) => {
+  let order_id  = req.query.pack_id;
+  let errors = false;
+  
+ 
+  if (order_id == null) {
+    order_id = 0;
+    errors = true;
+    // set flash message
+    req.flash('error', 'Please enter ID');
+    // render to add.ejs with flash message
+    res.redirect('/users');
+}
+  if(!errors){
+    dbCon.query('SELECT * FROM order_packaging WHERE pack_id = ?',order_id, (err, rows) => {
+      if (err) {
+          req.flash('error', err);
+          res.render('users/checkbyid', { pack_id: order_id ,data: '' , moment: moment});
+      }else{
+          
+          res.render('users/checkbyid', { pack_id: order_id, data: rows , moment: moment});
+      }
+    }) 
+  }
+})
 
 router.post('/check',(req, res, next) => {
   let order_id = req.body.order_id;
   let errors = false;
-  if (req.query.order_id != null){
+  if (req.query.pack_id != null){
     errors = true;
-    order_id = req.query.order_id;
+    order_id = req.query.pack_id;
   }
 
-  if (order_id.length === 0) {
+  if (order_id == null) {
     order_id = 0;
     errors = true;
     // set flash message
